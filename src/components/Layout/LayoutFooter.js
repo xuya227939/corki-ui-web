fee933163f64118d2af1cb5e3d57aad096fccacd
xuya227939 <hezhiqianye@gmail.com>
2023-06-17 17:16:50 +0800

    feat: update 备案号

--- src/components/Layout/LayoutFooter.js
+++ src/components/Layout/LayoutFooter.js
@@ -1,17 +1,18 @@
 /* eslint-disable react/jsx-no-target-blank */
-import React from 'react';
-import './LayoutFooter.less';
+import React from "react";
+import "./LayoutFooter.less";
 
 export default function LayoutFooter() {
-    return (
-        <footer styleName="layout-footer">
-            <p>
-                <a styleName="layoutfooter-icon icon-github" href="//github.com/xuya227939/corki-ui" target="_blank" />
-            </p>
-            <p>
-                Copyright © 2019 - {new Date().getFullYear()} Cherry Team
-                <br />
-            </p>
-        </footer>
-    );
+  return (
+    <footer styleName="layout-footer">
+      <p>
+        <a
+          styleName="layoutfooter-icon icon-github"
+          href="//github.com/xuya227939/corki-ui"
+          target="_blank"
+        />
+      </p>
+      <p>Copyright © 2019 - {new Date().getFullYear()} Cherry Team</p>
+    </footer>
+  );
 }
